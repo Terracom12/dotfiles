c331a945f0f7e771df72bc96bd976002c68c2897
Matthew Reese <awesomepop012@hotmail.com>
2025-09-10 16:24:50 -0700

    Update LazyVim and plugins, add required LSPs, remove some unneeded stuff

--- dot_config/nvim/lua/plugins/live-server.lua
+++ dot_config/nvim/lua/plugins/live-server.lua
@@ -1,3 +1,6 @@
+-- Not much of a web dev, and this is annoyingly asking me to install pnpm
+if true then return {} end
+
 return {
     "barrett-ruth/live-server.nvim",
     build = "pnpm add -g live-server",
--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -2,18 +2,36 @@ return {
     {
         "neovim/nvim-lspconfig",
 
+        version = "^1.0.0",
+
         opts = {
             servers = {
                 basedpyright = {
                     settings = {
-                        basedpyright = {
-                            analysis = {
-                                typeCheckingMode = "standard"
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+                        basedpyright = { analysis = { typeCheckingMode = "standard" } },
+                    },
+                },
+            },
+        },
+    },
+
+    {
+        "mason-org/mason-lspconfig.nvim",
+
+        version = "^1.0.0",
+
+        opts = {
+            ensure_installed = {
+                -- list of language servers to install by default
+                "lua_ls",
+                "clangd",
+                "basedpyright",
+                "bashls",
+            },
+        },
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+    },
 }
